faf733fa1e4543ae0cba6f28be5692d4cf65879f
Matt <matt@Matts-MacBook-Pro.local>
2021-09-05 23:32:46 +0800

    feature3

--- src/feature1.js
+++
@@ -1,3 +0,0 @@
-console.log("feature1");
-//test add new code to feature1
-//1 more line
--- src/feature2.js
+++
@@ -1,5 +0,0 @@
-console.log("feature2");
-conflict1 = () => {
-    console.log("conflictOnFeature2");
-}
-console.log("new line for pushing");
